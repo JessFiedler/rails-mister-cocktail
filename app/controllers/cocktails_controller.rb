a3ca68db687c5231ef4cc8bae04f1865c8da2716
Jessica Fiedler <jess.fiedler83860@gmail.com>
2019-05-24 15:37:22 +0200

    mrcocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,15 +2,16 @@ class CocktailsController < ApplicationController
   def new
     @cocktail = Cocktail.new
   end
-​
+
   def index
     @cocktails = Cocktail.all
   end
-​
+
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
-​
+
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
@@ -19,10 +20,10 @@ class CocktailsController < ApplicationController
       render :new
     end
   end
-​
+
   def edit
   end
-​
+
   def update
     if @cocktail.update(cocktail_params)
       redirect_to cocktail_path(@cocktail)
@@ -30,11 +31,16 @@ class CocktailsController < ApplicationController
       render :edit
     end
   end
-​
+
   def destroy
+    @cocktail = Cocktail.find(params[:id])
+    # @cocktail = Cocktail.find(params[:cocktail_id])
+    # @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
   private
-​
+
   def cocktail_params
     params.require(:cocktail).permit(:name)
   end
--- db/migrate/20190523150343_create_ingredients.rb
+++ db/migrate/20190523150343_create_ingredients.rb
@@ -2,8 +2,6 @@ class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.string :
-      t.string :string
 
       t.timestamps
     end
